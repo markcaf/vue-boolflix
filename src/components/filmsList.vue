4476d7412483ebc232d4f052d62d563712c30cd5
markcaf <marcocafassog@gmail.com>
2022-07-21 17:59:04 +0200

    Edit props to card, passed only single film object

--- src/components/filmsList.vue
+++ src/components/filmsList.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="filmList">
-    <filmCard v-for="(film, index) in films" :key="index" />
+  <div class="filmList d-flex flex-wrap justify-content-center">
+    <filmCard class="m-4" :film="film" v-for="(film, index) in films" :key="index" />
   </div>
 </template>
 
 <script>
+import filmCard from './filmCard.vue'
 export default {
     name: 'filmsList',
     components: {
