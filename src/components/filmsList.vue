25612e719bcb0c0b8f14b772a0841115e3398cd4
markcaf <marcocafassog@gmail.com>
2022-07-21 17:18:42 +0200

    Created component filmsList to contain filmCard

---
+++ src/components/filmsList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="filmList">
+    <filmCard v-for="(film, index) in films" :key="index" />
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'filmsList',
+    components: {
+        filmCard,
+    },
+
+    props: {
+        'films': {
+            required: true,
+            type: Array,
+        } 
+    },
+
+}
+</script>
+
+<style>
+
+</style>
