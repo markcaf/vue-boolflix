d04532bb245eb84dac2ce9c67e4c59a6a41c9a14
markcaf <marcocafassog@gmail.com>
2022-07-21 17:17:27 +0200

    Added props and child components

---
+++ src/components/Main.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <filmsList :films="films" />
+            </div>
+        </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import filmsList from './filmsList.vue'
+import filmCard from './filmCard.vue'
+
+export default {
+    name: 'Main',
+    components: {
+        filmsList,
+        filmCard,
+    },
+
+    props: {
+        'films': {
+            required: true,
+            type: Array,
+        } 
+    },
+
+}
+</script>
+
+<style>
+
+</style>
