08c84ccb769f43626c2c9ecca7b1b97cb72601b3
markcaf <marcocafassog@gmail.com>
2022-07-21 17:59:34 +0200

    Dynamic generation of single film data

--- src/components/filmCard.vue
+++ src/components/filmCard.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="col-2">
     <ul>
-        <li>titolo</li>
-        <li>titolo originale</li>
-        <li>lingua</li>
-        <li>voto</li>
+        <li><strong>Titolo:</strong> {{ film.title }}</li>
+        <li><strong>Titolo originale:</strong> {{ film.original_title }}</li>
+        <li><strong>Lingua:</strong> {{ film.original_language }}</li>
+        <li><strong>Voto:</strong> {{ film.vote_average }}</li>
     </ul>
   </div>
 </template>
@@ -12,6 +12,13 @@
 <script>
 export default {
     name: 'filmCard',
+
+    props: {
+        'film': {
+            required: true,
+            type: Object,
+        } 
+    },
 
 }
 </script>
