4c27ea4284acfcf150a31b03040b338487e1414e
markcaf <marcocafassog@gmail.com>
2022-07-21 16:30:31 +0200

    Created the imported header component w/ searchbar

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <div class="container-fluid p-4 border">
+        <div class="row justify-content-between">
+            <div class="col-6 logo">
+                <h3>Boolflix</h3>
+            </div>
+
+            <div class="col-4">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Inserisci il titolo di un film" aria-label="films" aria-describedby="basic-addon1">
+                    <span class="input-group-text" id="basic-addon1">Cerca</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
