bc0741db467cf9ea185fa2307c21a2d205894ecd
markcaf <marcocafassog@gmail.com>
2022-07-21 17:18:59 +0200

    Created the component for the single film card

---
+++ src/components/filmCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul>
+        <li>titolo</li>
+        <li>titolo originale</li>
+        <li>lingua</li>
+        <li>voto</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'filmCard',
+
+}
+</script>
+
+<style>
+
+</style>
