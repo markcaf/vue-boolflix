70bf9571e8be275fc7824f1c1b902fa622074798
markcaf <marcocafassog@gmail.com>
2022-07-22 11:12:16 +0200

    Import lang-code-flags to transform string to flag

--- src/components/filmCard.vue
+++ src/components/filmCard.vue
@@ -3,15 +3,19 @@
     <ul>
         <li><strong>Titolo:</strong> {{ film.title }}</li>
         <li><strong>Titolo originale:</strong> {{ film.original_title }}</li>
-        <li><strong>Lingua:</strong> {{ film.original_language }}</li>
+        <li><strong>Lingua: </strong><lang-flag :iso="film.original_language" /></li>
         <li><strong>Voto:</strong> {{ film.vote_average }}</li>
     </ul>
   </div>
 </template>
 
 <script>
+import LangFlag from 'vue-lang-code-flags';
 export default {
     name: 'filmCard',
+    components: {
+        LangFlag,
+    },
 
     props: {
         'film': {
