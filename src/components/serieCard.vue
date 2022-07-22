e9081aef6fbcb04b730df3fd78111ceb9176fb82
markcaf <marcocafassog@gmail.com>
2022-07-22 12:49:31 +0200

    Created the component for the single tv serie card

---
+++ src/components/serieCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col-2">
+    <ul>
+        <li><strong>Titolo:</strong> {{ serie.name }}</li>
+        <li><strong>Titolo originale:</strong> {{ serie.original_name }}</li>
+        <li><strong>Media:</strong> {{ serie.media_type }}</li>
+        <li><strong>Lingua: </strong><lang-flag :iso="serie.original_language" /></li>
+        <li><strong>Voto:</strong> {{ serie.vote_average }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import LangFlag from 'vue-lang-code-flags';
+export default {
+    name: 'serieCard',
+    components: {
+        LangFlag,
+    },
+
+    props: {
+        'serie': {
+            required: true,
+            type: Object,
+        } 
+    },
+
+}
+</script>
+
+<style>
+
+</style>
