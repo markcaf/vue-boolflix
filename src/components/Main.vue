efdaadcc538766aba2f1f3def69f00db26acff1c
markcaf <marcocafassog@gmail.com>
2022-07-22 12:47:42 +0200

    Added import and props for seriesList component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,15 @@
 <template>
-  <main>
+  <main class="py-3">
     <div class="container">
         <div class="row">
             <div class="col">
                 <filmsList :films="films" />
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <seriesList :series="series" />
             </div>
         </div>
     </div>
@@ -12,18 +18,25 @@
 
 <script>
 import filmsList from './filmsList.vue'
+import seriesList from './seriesList.vue'
 
 export default {
     name: 'Main',
     components: {
         filmsList,
+        seriesList,
     },
 
     props: {
         'films': {
             required: true,
             type: Array,
-        } 
+        },
+        
+        'series': {
+            required: true,
+            type: Array,
+        },
     },
 
 }
