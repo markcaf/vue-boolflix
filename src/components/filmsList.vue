67c81b07b0c24dd777579feb51f7c85a754a8873
markcaf <marcocafassog@gmail.com>
2022-07-22 12:50:04 +0200

    Added the component's heading

--- src/components/filmsList.vue
+++ src/components/filmsList.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="filmList d-flex flex-wrap justify-content-center">
-    <filmCard class="m-4" :film="film" v-for="(film, index) in films" :key="index" />
-  </div>
+    <section class="mt-3">
+        <h1 class="col-2 ps-3 text-white bg-dark">Film</h1>
+        <div class="filmList d-flex flex-wrap">
+            <filmCard class="m-3" :film="film" v-for="(film, index) in films" :key="index" />
+        </div>
+    </section>
 </template>
 
 <script>
