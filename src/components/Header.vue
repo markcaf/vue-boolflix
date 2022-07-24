7fd6333d152680cbf33487479f5c30bd487cbb18
markcaf <marcocafassog@gmail.com>
2022-07-25 00:30:01 +0200

    Added header styling in custom CSS and Bootstrap

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,16 @@
 <template>
   <header>
-    <div class="container-fluid p-4 border">
+    <div class="container-fluid p-4">
         <div class="row justify-content-between">
             <div class="col-6 logo">
-                <h3>Boolflix</h3>
+                <h3 class="text-danger text-uppercase fw-bold">Boolflix</h3>
             </div>
 
             <div class="col-4">
                 <div class="input-group">
                     <input v-model.trim="searchInput" @keyup.enter="$emit('search', searchInput)"
-                    type="text" class="form-control" placeholder="Inserisci il titolo di un film" aria-label="films" aria-describedby="basic-addon1">
-                    <span @click.prevent="$emit('search', searchInput)" class="input-group-text" id="basic-addon1">Cerca</span>
+                    type="text" class="form-control" placeholder="Inserisci il titolo di un film o serie tv" aria-label="films" aria-describedby="basic-addon1">
+                    <span @click.prevent="$emit('search', searchInput)" class="input-group-text bg-danger text-white border-0" id="basic-addon1">Cerca</span>
                 </div>
             </div>
         </div>
@@ -31,10 +31,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .input-group{
-        span{
-            cursor: pointer;
+    header{
+        background-color: #101010;
+        
+        .input-group{
+            span{
+                cursor: pointer;
+            }
         }
+
     }
+    
 
 </style>
